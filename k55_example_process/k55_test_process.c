45ef491210e826b001b149774b84f66977d19e40
Josh Schiavone (josh0xA) <jshschiavone@gmail.com>
2021-01-12 19:29:07 -0500

    licence for exemplar

--- k55_example_process/k55_test_process.c
+++ k55_example_process/k55_test_process.c
@@ -1,3 +1,12 @@
+/* Copyright (C) 2021 Josh Schiavone - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, visit : https://opensource.org/licenses/MIT
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
