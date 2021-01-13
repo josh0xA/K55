696aa02c7ea18c0dfcca9a4c2e16cfd1a072e2f6
Josh Schiavone (josh0xA) <jshschiavone@gmail.com>
2021-01-12 22:35:41 -0500

    native architecture flag

--- build-install.sh
+++ build-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo g++ -std=c++14 -I . -I ./src *.cc src/*.cc -w -o K55
+sudo g++ -std=c++14 -I . -I ./src *.cc src/*.cc -march=native -w -o K55
 sudo gcc -z execstack k55_example_process/k55_test_process.c -fno-stack-protector -o k55_example_process/k55_test_process
